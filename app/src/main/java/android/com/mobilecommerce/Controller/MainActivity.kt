9742e9cce63356942c5ef59185b7858606811a06
Dexter John Datul <ddatul@almadaclinic.com>
2017-11-30 10:59:50 +0300

    Created Listview

--- app/src/main/java/android/com/mobilecommerce/Controller/MainActivity.kt
+++ app/src/main/java/android/com/mobilecommerce/Controller/MainActivity.kt
@@ -1,13 +1,23 @@
 package android.com.mobilecommerce.Controller
 
+import android.com.mobilecommerce.Model.Category
 import android.com.mobilecommerce.R
+import android.com.mobilecommerce.Services.DataService
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/android/com/mobilecommerce/Model/Category.kt
+++ app/src/main/java/android/com/mobilecommerce/Model/Category.kt
@@ -4,4 +4,10 @@ package android.com.mobilecommerce.Model
  * Created by Dexter John Datul on 29/11/2017.
  */
 
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+}
+
+
