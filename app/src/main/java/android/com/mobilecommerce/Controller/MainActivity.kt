c70cb275ea137913ea09e49914e322d4a574727e
Dexter John Datul <ddatul@almadaclinic.com>
2017-11-30 12:05:46 +0300

    Created CustomListView

--- app/src/main/java/android/com/mobilecommerce/Controller/MainActivity.kt
+++ app/src/main/java/android/com/mobilecommerce/Controller/MainActivity.kt
@@ -1,22 +1,21 @@
 package android.com.mobilecommerce.Controller
 
-import android.com.mobilecommerce.Model.Category
+import android.com.mobilecommerce.Adapters.CategoryAdapter
 import android.com.mobilecommerce.R
 import android.com.mobilecommerce.Services.DataService
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
     }
