9439c99a6971b7f5f0cd31ba8f1d84191f8b686e
Dexter John Datul <ddatul@almadaclinic.com>
2017-12-10 20:59:17 +0300

    Created Recyclerview

---
+++ app/src/main/java/android/com/mobilecommerce/Adapters/CategoryRecycleAdapter.kt
@@ -0,0 +1,43 @@
+package android.com.mobilecommerce.Adapters
+
+import android.com.mobilecommerce.Model.Category
+import android.com.mobilecommerce.R
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+/**
+ * Created by Dexter John Datul on 10/12/2017.
+ */
+class CategoryRecycleAdapter(val context: Context, val categories: List<Category>) : RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
+
+    override fun onBindViewHolder(holder: Holder?, position: Int) {
+        holder?.bindCategory(categories[position], context)
+    }
+
+    override fun getItemCount(): Int {
+     return categories.count()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): Holder {
+        val view = LayoutInflater.from(context)
+                .inflate(R.layout.category_list_item, parent, false)
+        return Holder(view)
+    }
+
+    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView){
+        private val categoryImage = itemView?.findViewById<ImageView>(R.id.categoryImage)
+        private val categoryName = itemView?.findViewById<TextView>(R.id.categoryName)
+
+        fun bindCategory(category: Category, context: Context){
+            val resourceID = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+            categoryImage?.setImageResource(resourceID)
+            categoryName?.text = category.title
+
+        }
+    }
+}
--- app/src/main/java/android/com/mobilecommerce/Controller/MainActivity.kt
+++ app/src/main/java/android/com/mobilecommerce/Controller/MainActivity.kt
@@ -1,23 +1,27 @@
 package android.com.mobilecommerce.Controller
 
-import android.com.mobilecommerce.Adapters.CategoryAdapter
+import android.com.mobilecommerce.Adapters.CategoryRecycleAdapter
 import android.com.mobilecommerce.R
 import android.com.mobilecommerce.Services.DataService
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: CategoryAdapter
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
-     //   categoryListView.adapter = adapter
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
+        categoryListView.adapter = adapter
 
+        val layoutManager = LinearLayoutManager(this)
 
-
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
     }
 }
